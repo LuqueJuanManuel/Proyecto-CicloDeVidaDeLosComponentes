cc686832f7f3d3fb7c8e0a3dd17688bfd6ad9ba5
JuanLuque <juanluquejml@gmail.com>
2023-06-23 15:45:52 -0300

    Proyecto Integrador - Digital House - El ciclo de vida de los componentes

---
+++ README.md
@@ -0,0 +1,14 @@
+# Proyecto Integrador - El ciclo de vida de los componentes.
+
+## Utilizaremos de base los siguientes proyectos:
+
+* PROYECTO CREADO CON REACT: recordemos instalar todas las dependencias del proyecto, ejecutando la instrucción npm install.
+
+* Para activar el servidor debemos ejecutar el comando npm start y finalmente en el navegador ejecutar: http://localhost:3000/.
+
+****************************************************************************************
+
+* PROYECTO CREADO CON EXPRESS: recordemos instalar todas las dependencias del proyecto, ejecutando la instrucción npm install. Además, aprovecharemos la base de datos movies_db (no olvidemos activar el servicio de MySQL en nuestro equipo). De esta manera, todo funcionará correctamente.
+
+* Para activar el servidor debemos ejecutar el comando npm test y finalmente en el
+navegador ejecutar: http://localhost:3001/.
---
+++ dashboard/src/components/GenresInDb.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import Genre from './Genre';
+
+
+
+
+class GenresInDb extends Component {
+    
+    constructor(props) {
+        super(props)
+        this.state = {
+            genresList: [],
+        }
+    }
+    
+    componentDidMount() {
+        fetch('/api/genres')
+            .then(respuesta => {
+                console.log(respuesta)
+                return respuesta.json()
+                
+            })
+            .then(genres => {
+                console.log(genres)
+                this.setState({ genresList: genres.data })
+                console.log(genres)
+            })
+            .catch(error => console.log(error))
+    }
+    render() {
+        return (
+            <React.Fragment>
+            {/*<!-- Categories in DB -->*/}
+            <div className="col-lg-6 mb-4">
+                <div className="card shadow mb-4">
+                    <div className="card-header py-3">
+                        <h6 className="m-0 font-weight-bold text-gray-800">Genres in Data Base</h6>
+                    </div>
+                    <div className="card-body">
+                        <div className="row">
+                            {
+                                this.state.genresList.map((genre, index) => {
+                                    return <Genre  {...genre} key={index} />
+                                })
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </React.Fragment>
+        )
+    }
+};
+
+export default GenresInDb;
